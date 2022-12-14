817e42c5e72ca1273967d3ad3e700501b7801ff8
achocho <adrian.peshev2003@gmail.com>
2022-12-14 17:52:01 +0200

    Urok1-update

--- register.php
+++ register.php
@@ -5,6 +5,23 @@ if($_SERVER['REQUEST_METHOD']=="POST")
 {
   $username=$_POST['username'];
   $password=$_POST['password'];
+  $id="";
+ $choice=0;
+    for($i=0;$i<10;$i++)
+    {
+        $choice=rand(1,3);
+        if($choice==1){
+        $id.=chr(rand(97,122));
+        }
+        else if($choice==2){
+        $id.=chr(rand(65,90));
+        }
+        else if($choice==3){
+        $id.=chr(rand(48,57));
+        }
+    }
+    
+    echo "<script>console.log('$id')</script>";
   $sql="Insert into users(Username,Password) Values('$username','$password')";
    mysqli_query($conn,$sql);
 
